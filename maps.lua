4f03c840bd2b95a0afe692923abb0e4a9e228f89
npat15 <nicholas.r.patilsen@vanderbilt.edu>
2020-07-31 18:02:56 -0400

    created maps folder

--- maps.lua
+++ maps.lua
@@ -1,13 +1,13 @@
 return {
 
     {   
-        filename = 'map.lua',
+        filename = 'maps/map.lua',
         toDraw = {"Tile Layer 1", "Buildings"},
-        player = Character('Player', 'player', 0, 0, 16, 32, "tiles/gfx/character.png", true, 'map.lua', {}),
+        player = Character('Player', 'player', 0, 0, 16, 32, "maps/tiles/gfx/character.png", true, 'maps/map.lua', {}),
         npcs =      {
-                        Character('NPC', 'npc', 0, 0, 16, 32, "tiles/gfx/NPC_test.png", true, 'map.lua', {"Hi!", "I'm happy!"}),
-                        Character('NPC1', 'npc', 0, 0, 16, 32, "tiles/gfx/NPC_test.png", true, 'map.lua', {'How are you?'}),
-                        Character('NPC2', 'npc', 0, 0, 16, 32, "tiles/gfx/NPC_test.png", true, 'map.lua', {'You!', "You're not supposed to be in here!"}),
+                        Character('NPC', 'npc', 0, 0, 16, 32, "maps/tiles/gfx/NPC_test.png", true, 'maps/map.lua', {"Hi!", "I'm happy!"}),
+                        Character('NPC1', 'npc', 0, 0, 16, 32, "maps/tiles/gfx/NPC_test.png", true, 'maps/map.lua', {'How are you?'}),
+                        Character('NPC2', 'npc', 0, 0, 16, 32, "maps/tiles/gfx/NPC_test.png", true, 'maps/map.lua', {'You!', "You're not supposed to be in here!"}),
                     }, 
         triggers = {
                         Trigger({'Shed', 'ShedObjects'}, 23, 23, 9, 57, {"Tile Layer 1", "Buildings"}),
