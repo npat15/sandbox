78d13017141f119b74d88bf0ef7ab14a011b21f1
npat15 <nicholas.r.patilsen@vanderbilt.edu>
2020-08-01 21:58:49 -0400

    player can move across maps

---
+++ MapTrigger.lua
@@ -0,0 +1,15 @@
+MapTrigger = Class{}
+
+function MapTrigger:init(tile_x0, tile_y0, future_index)
+    self.x0 = tile_x0
+    self.y0 = tile_y0
+    self.future_index = future_index
+end
+
+function MapTrigger:enter()
+    print('found')
+    g_map_index = self.future_index
+    make_map(g_map_index)
+    sounds['door']:play()
+end
+
